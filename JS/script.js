78494b7b10ce99537eabe3a6d93e2428c7c7f5b1
Yusudime <yusudimelucasnascimentosousa@gmail.com>
2024-09-18 19:25:01 -0300

    hello

--- JS/script.js
+++ JS/script.js
@@ -1,2 +1,21 @@
-titulo = document.querySelector('#titulo');
-descricao = document.querySelector('#descricao');
+const btn = document.querySelector('#btn');
+const titulo = document.querySelector('#titulo');
+const input = document.querySelector('#input');
+const descricao = document.querySelector('#descricao');
+
+const validarTitulo=()=>{
+    const re = /[a-zA-Z]/;
+    const valor = titulo.value;
+     if (!re.test(valor.slice(-1))){
+        titulo.value = valor.slice(0,-1);
+     }}
+titulo.addEventListener('keyup',(e)=>{
+    console.log(" Parou de Apertou uma tecla");
+    validarTitulo();
+});
+titulo.addEventListener('blur',(e)=>{
+    console.log("Desfocou");
+    if(titulo.value.length > 250){
+        alert("titulo muito grande");
+       
+    }});
