122ec5fdb14e94dae6b0ad05837e39e554413059
Yusudime <yusudimelucasnascimentosousa@gmail.com>
2024-09-25 17:48:08 -0300

    hello

--- models.py
+++ models.py
@@ -1,6 +1,10 @@
 class User(db.model):
-    descricao = db.Column(db.Integer, primary_key=True)
-    titulo = db.Column(db.String(200), nullable=False)
+    descricao = db.Column(db.Integer,nullable=False )
+    db.session.add(descricao)
+    db.session.commit()
+    titulo = db.Column(db.String(200), nullable=False,primary_key=True)
+    db.session.add(titulo)
+    db.session.commit()
 def __init__(self, descricao, titulo):
     self.descricao = descricao
     self.titulo = titulo
